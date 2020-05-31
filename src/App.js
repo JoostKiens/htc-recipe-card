9f68fdca62e59d9378bd1a4386848caa428b3d7e
joostkiens <me@joostkiens.com>
2020-05-31 14:46:26 +0200

    Formatting

--- src/App.js
+++ src/App.js
@@ -24,26 +24,26 @@ function App() {
         <li className='List-item'>
           <CoverItem {...{ image, setAppState }} />
         </li>
-      {times(appState.ingredientItemsCount).map((x, index) =>
-        <li className='List-item' key={index}>
-          <ContentItem
-            title={'Ingredients'}
-            stateKey='ingredientItemsCount'
-            itemCount={appState.ingredientItemsCount}
-            {...{ index, image, add, remove }}
-          />
-        </li>
-      )}
-      {times(appState.methodItemsCount).map((_, index) =>
-        <li className='List-item' key={index}>
-          <ContentItem
-            title={'- How to cook -'}
-            stateKey='methodItemsCount'
-            itemCount={appState.methodItemsCount}
-            {...{ index, image, add, remove }}
-          />
-        </li>
-      )}
+        {times(appState.ingredientItemsCount).map((x, index) =>
+          <li className='List-item' key={index}>
+            <ContentItem
+              title={'Ingredients'}
+              stateKey='ingredientItemsCount'
+              itemCount={appState.ingredientItemsCount}
+              {...{ index, image, add, remove }}
+            />
+          </li>
+        )}
+        {times(appState.methodItemsCount).map((_, index) =>
+          <li className='List-item' key={index}>
+            <ContentItem
+              title={'- How to cook -'}
+              stateKey='methodItemsCount'
+              itemCount={appState.methodItemsCount}
+              {...{ index, image, add, remove }}
+            />
+          </li>
+        )}
       </ul>
     </div>
   )
